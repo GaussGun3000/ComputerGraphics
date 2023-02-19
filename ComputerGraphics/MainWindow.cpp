a7b93f3dbf218c23820c7301d2d0ec310a4ad8e5
vva07 <vva0779@yandex.ru>
2023-02-19 21:15:18 +0300

    GUI layout complete

--- ComputerGraphics/MainWindow.cpp
+++ ComputerGraphics/MainWindow.cpp
@@ -4,6 +4,8 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    ui.formLayout->setSizeConstraint(QLayout::SetFixedSize);
+    ui.verticalLayout_3->addLayout(ui.formLayout);
 }
 
 MainWindow::~MainWindow()
