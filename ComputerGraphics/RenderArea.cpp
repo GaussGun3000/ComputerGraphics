4a9be901a83bfa8f83957cec3dbf3faf414bdcf6
immariav <mariavolynkina03@gmail.com>
2023-02-20 21:01:23 +0300

    started making axes marking

--- ComputerGraphics/RenderArea.cpp
+++ ComputerGraphics/RenderArea.cpp
@@ -14,8 +14,21 @@ void RenderArea::paintEvent(QPaintEvent* event)
 	QPainter painter(this);
 	QPoint offset = getOffset();
 	QPen penRectangle(QColor("black"));
-	painter.drawLine(0, offset.y(), width(), offset.y()); // x axis
-	painter.drawLine(offset.x(), 0, offset.x(), height()); // y axis
+
+	int w = width(); int h = height(); // getting display size
+
+	painter.drawLine(0, offset.y(), w, offset.y()); // x axis
+	painter.drawLine(offset.x(), 0, offset.x(), h); // y axis
+
+	//painter.drawLine(0, offset.y() - 4, 0, offset.y() + 4);
+
+	for (int coordX = w/2; coordX < w; coordX += 20) // marking x axis
+	{
+		painter.drawLine(coordX, offset.y() - 4, coordX, offset.y() + 4);
+		painter.drawLine(w - coordX, offset.y() - 4, w - coordX, offset.y() + 4);
+		//painter.drawLine(-width() / 2 + coordX, -2, -width() / 2 + coordX, 2);
+	}
+
 	painter.setPen(penRectangle);
 	painter.drawLines(getRectLines(QPointF(-10 + offset.x(), 10 + offset.y()), QPointF(20 + offset.x(), 10 + offset.y()),
 		QPointF(20 + offset.x(), -10 + offset.y()), QPointF(-10 + offset.x(), -10 + offset.y()))); 
