6af3976b114a013233b1fd507e77d83d076f6890
vva07 <vva0779@yandex.ru>
2023-04-03 20:40:49 +0300

    reversed the offset back

--- ComputerGraphics/Lab2/RenderArea.cpp
+++ ComputerGraphics/Lab2/RenderArea.cpp
@@ -107,6 +107,6 @@ QPoint RenderArea::getOffset()
 *	Calculate cooridnates offset to shift (0,0) for markers to the center of the widget
 */
 {
-	return QPoint(this->width() / 2, -this->height() / 2);
+	return QPoint(this->width() / 2, this->height() / 2);
 }
 
