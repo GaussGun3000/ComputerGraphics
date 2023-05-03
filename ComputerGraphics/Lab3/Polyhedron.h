e5131300fef75dfa9f700c4d75fa1a50d013e83b
vva07 <vva0779@yandex.ru>
2023-05-03 23:02:07 +0300

    Polyhedron untested (with mesh generation)

--- ComputerGraphics/Lab3/Polyhedron.h
+++ ComputerGraphics/Lab3/Polyhedron.h
@@ -1,5 +1,36 @@
 #pragma once
-class Polyhedron
-{
+#include <qvector.h>
+#include <qvector3d.h>
+
+class Triangle {
+public:
+    Triangle(const QVector3D& v1, const QVector3D& v2, const QVector3D& v3)
+        : vertices{ v1, v2, v3 } {}
+
+    const QVector3D& operator[](int i) const { return vertices[i]; }
+    QVector3D& operator[](int i) { return vertices[i]; }
+
+private:
+    QVector3D vertices[3];
 };
+
+class Polyhedron {
+public:
+    Polyhedron(QVector<QVector3D> vertices, QVector<QVector<int>> faces)
+        : vertices(vertices), faces(faces) {}
+
+    void generateRandomPolyhedron();
+
+    QVector<QVector3D> getVertices() const { return vertices; }
+    QVector<Triangle> getMesh() const;
+
+
+private:
+    QVector<QVector3D> vertices;
+    QVector<QVector<int>> faces;
+    const uint32_t VERT_COUNT = 16;
+};
+
+
+
 
--- ComputerGraphics/Lab3/main.cpp
+++ ComputerGraphics/Lab3/main.cpp
@@ -2,10 +2,21 @@
 #include <QtWidgets/QApplication>
 
 
-int main(int argc, char *argv[])
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+//}
+
+#include <QDebug>
+#include "polyhedron.h"
+
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    Polyhedron poly = Polyhedron::generateRandomPolyhedron(10,);
+    QVector3D c = poly.centroid();
+    qDebug() << "Centroid: (" << c.x() << ", " << c.y() << ", " << c.z() << ")";
+    return 0;
 }
