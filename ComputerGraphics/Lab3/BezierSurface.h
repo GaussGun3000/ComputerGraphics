5f3c51120fc99eb77f7dc5a47b385c4e800e8af8
immariav <mariavolynkina03@gmail.com>
2023-05-03 23:41:28 +0300

    BezierSurface class declaration

--- ComputerGraphics/Lab3/BezierSurface.h
+++ ComputerGraphics/Lab3/BezierSurface.h
@@ -1,5 +1,18 @@
 #pragma once
-class BezierSurface
-{
-};
 
+#include <QVector>
+#include <QVector3D>
+
+class BezierSurface {
+public:
+    BezierSurface();
+    BezierSurface(const QVector<QVector3D>& controlPoints);
+
+    void setControlPoints(const QVector<QVector3D>& controlPoints);
+    void calculateSurface(int numUSteps, int numVSteps);
+    void render();
+
+private:
+    QVector<QVector3D> m_controlPoints;
+    QVector<QVector3D> m_surfacePoints;
+};
