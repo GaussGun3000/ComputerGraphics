f7cf4f485844ba8964e34da2a1fe79aee917971a
vva07 <vva0779@yandex.ru>
2023-05-06 17:17:51 +0300

    Billinear surface interpolation without rotation

--- ComputerGraphics/Lab3/BilinearSurface.cpp
+++ ComputerGraphics/Lab3/BilinearSurface.cpp
@@ -25,3 +25,30 @@ QVector<QVector3D> BilinearSurface::cornerPoints() const
 {
     return m_cornerPoints;
 }
+
+QVector<QVector3D> BilinearSurface::interpolate(int numStepsU, int numStepsV) const
+{
+    QVector<QVector3D> surfacePoints;
+
+    // Make sure we have at least 4 steps in each direction
+    numStepsU = qMax(4, numStepsU);
+    numStepsV = qMax(4, numStepsV);
+
+    // Get the four corner points of the bilinear surface
+    QVector3D p1 = m_cornerPoints.at(0);
+    QVector3D p2 = m_cornerPoints.at(1);
+    QVector3D p3 = m_cornerPoints.at(2);
+    QVector3D p4 = m_cornerPoints.at(3);
+
+    // Interpolate the surface points using bilinear interpolation
+    for (int i = 0; i <= numStepsU; i++) {
+        float u = i / static_cast<float>(numStepsU);
+        for (int j = 0; j <= numStepsV; j++) {
+            float v = j / static_cast<float>(numStepsV);
+            QVector3D point = (1 - u) * (1 - v) * p1 + u * (1 - v) * p2 + u * v * p3 + (1 - u) * v * p4;
+            surfacePoints.append(point);
+        }
+    }
+
+    return surfacePoints;
+}
