6c3037703d0dc1a51e4e70e7d4c04e6f71ef2660
vva07 <vva0779@yandex.ru>
2023-05-07 18:10:21 +0300

    status bar messages added

--- ComputerGraphics/Lab3/MainWindow.cpp
+++ ComputerGraphics/Lab3/MainWindow.cpp
@@ -14,6 +14,7 @@ MainWindow::~MainWindow()
 void MainWindow::saveInputData(QVector<QVector3D>& points)
 {
     ui.renderArea->surface->setCornerPoints(points);
+    ui.statusBar->showMessage("Corner points saved. Press update to render new surface");
 }
 
 QVector<QVector3D> MainWindow::getSurfaseData()
@@ -25,6 +26,7 @@ QVector<QVector3D> MainWindow::getSurfaseData()
 void MainWindow::updateButtonClicked()
 {
     ui.renderArea->updateSurface(0, 0);
+    ui.statusBar->showMessage("Surface updated");
 }
 
 
