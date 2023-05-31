4797f607dddc861f253944537055f8ea032a07a8
immariav <mariavolynkina03@gmail.com>
2023-05-31 23:42:51 +0300

    edited calculation of max and min vertices

--- ComputerGraphics/Lab5/InputWindow.cpp
+++ ComputerGraphics/Lab5/InputWindow.cpp
@@ -25,21 +25,29 @@ void InputWindow::saveButtonClicked()
     mainWindow->saveInputData(settings);
 }
 
-void InputWindow::eMaxSpinBoxValueChanged()
+void InputWindow::fMaxSpinBoxValueChanged()
 {
-    if(ui.spinBox_fMax->value() == 2 || ui.spinBox_fMax->value() == 3)
+    uint32_t fMax = ui.spinBox_fMax->value();
+    if(fMax >= 2 && fMax <=4)
+    {
+        ui.label_vMin->setText("4");
         ui.label_vMax->setText("4");
+    }
+    else if (fMax == 5)
+    {
+        ui.label_vMin->setText("6");
+        ui.label_vMax->setText("6");
+    }
     else
-        ui.label_vMax->setText(QString::number(ui.spinBox_fMax->value()));
-    ui.spinBox_fMin->setMaximum(ui.spinBox_fMax->value());
+    {
+        ui.label_vMin->setText(QString::number(calcMinV(fMax)));
+        ui.label_vMax->setText(QString::number(fMax));
+    }
+    ui.spinBox_fMin->setMaximum(fMax);
 }
 
-void InputWindow::eMinSpinBoxValueChanged()
+void InputWindow::fMinSpinBoxValueChanged()
 {
-    if (ui.spinBox_fMin->value() == 2 || ui.spinBox_fMin->value() == 3)
-        ui.label_vMin->setText("4");
-    else
-        ui.label_vMin->setText(QString::number(ui.spinBox_fMin->value()));
     ui.spinBox_fMax->setMinimum(ui.spinBox_fMin->value());
 }
 
@@ -52,5 +60,30 @@ void InputWindow::fillOldValues()
     ui.spinBox_fMax->setValue(settings.max_faces);
     ui.label_vMax->setText(QString::number(settings.max_vertices));
     ui.label_vMin->setText(QString::number(settings.min_vertices));
+}
+
+uint32_t InputWindow::factorial(uint32_t n)
+{
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+uint32_t InputWindow::calcMinV(uint32_t maxF)
+{
+    uint32_t m = 4; 
+
+    while (true) {
+        uint32_t numerator = factorial(m);
+        uint32_t denominator = factorial(3) * factorial(m - 3);
+
+        if (numerator >= (denominator * maxF))
+            return m + 1;
+
+        m++;  
+    }
 }
 
--- ComputerGraphics/Lab5/InputWindow.h
+++ ComputerGraphics/Lab5/InputWindow.h
@@ -10,11 +10,14 @@ public:
  
 public slots:
     void saveButtonClicked();
-    void eMaxSpinBoxValueChanged();
-    void eMinSpinBoxValueChanged();
+    void fMaxSpinBoxValueChanged();
+    void fMinSpinBoxValueChanged();
 
 private:
     void fillOldValues();
+
+    uint32_t factorial(uint32_t n);
+    uint32_t calcMinV(uint32_t maxF);
 
     Ui::InputWindow ui;
     // Add any additional functions for the secondary window here
